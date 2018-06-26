d6fe52dd84be79ed2e02a4962fa6e618bfd3d9d3
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-06-26 20:19:38 +0800

    add files

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const { upload } = require('./src/build/Release/sync_req')
 module.exports = {
   upload
 }
+
+// console.log(upload('http://www.baidudu.com', '/Users/ytx/Projects/sync-req/index.js'))
--- src/curl.cc
+++ src/curl.cc
@@ -30,41 +30,52 @@ namespace sr_curl
 //   return "ok";
 // }
 
+struct MemoryStruct
+{
+  char *memory;
+  size_t size;
+};
+
 const char *upload(const char *url, const char *filePath)
 {
   CURL *curl;
   CURLcode res;
   char *errMsg = NULL;
+  struct curl_httppost *formpost = NULL;
+  struct curl_httppost *lastptr = NULL;
+  struct MemoryStruct chunk;
+
+  // curl_global_init(CURL_GLOBAL_ALL);
+
+  curl_formadd(&formpost, &lastptr, CURLFORM_COPYNAME, "files", CURLFORM_FILE, filePath, CURLFORM_END);
 
   curl = curl_easy_init();
   if (!curl)
     return "无法构建curl对象";
-  curl_easy_setopt(curl, CURLOPT_URL, url);
-  curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-  curl_easy_setopt(curl, CURLOPT_PUT, 1L);
 
-  FILE *fp = fopen(filePath, "rb");
-  if (!fp)
-  {
-    sprintf(errMsg, "文件无法打开文件：%s", filePath);
-    return errMsg;
-  }
-  fseek(fp, 0, SEEK_END); // 将文件指针移动文件结尾
-  long fsize = ftell(fp);
-
-  curl_easy_setopt(curl, CURLOPT_READDATA, fp);
-  curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, fsize);
+  // 解决100-continue问题
+  struct curl_slist *headers = 0;
+  headers = curl_slist_append(headers, "Expect:");
+  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
   curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+  curl_easy_setopt(curl, CURLOPT_URL, url);
+  curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+  // TODO get response body, see: https://curl.haxx.se/libcurl/c/getinmemory.html
+  // curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
   res = curl_easy_perform(curl);
 
-  fclose(fp);
+  // 释放资源
+  curl_easy_cleanup(curl);
+  curl_formfree(formpost);
+  curl_slist_free_all(headers);
+
   if (res != CURLE_OK)
   {
     sprintf(errMsg, "%s", curl_easy_strerror(res));
     return errMsg;
   }
-
   return "ok";
 }
-} 
+} // namespace sr_curl
